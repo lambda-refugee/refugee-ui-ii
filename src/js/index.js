32d9e193c19f7c4c1db25779e8a069e0490b193c
Peter Murphy <peter.francis.murphy@gmail.com>
2019-02-05 12:33:03 -0500

    Toggles navbar on below the fold

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,21 @@
 main = () => {
-  console.log("JavaScript!!!")
+  const getHeaderHeight = () =>
+    +getComputedStyle(document.querySelector("header")).height.slice(0, -2)
+  const navBarContainer = document.querySelector(".nav-outer-container")
+
+  const showNavBar = () => {
+    const headerHeight = getHeaderHeight()
+    window.scrollY > headerHeight
+      ? navBarContainer.classList.add("nav-show")
+      : navBarContainer.classList.remove("nav-show")
+  }
+
+  // If user resizes the height of window, the navbar will still
+  // be activated below the fold for whatever window height
+  document.addEventListener("resize", () => {
+    document.onscroll = showNavBar
+  })
+  document.onscroll = showNavBar
 }
 
 window.onload = main
