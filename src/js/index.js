270c74d7b8f60e2f3fbf336ea3071e6b619a6de9
Peter Murphy <peter.francis.murphy@gmail.com>
2019-02-06 23:21:10 -0500

    This is a super hacky fix, but navbar works in both mobile/tablet (with hamburger) and desktop views (with inline links)

--- src/js/index.js
+++ src/js/index.js
@@ -48,26 +48,74 @@ main = () => {
   document.onscroll = showNavBar
 
   const dropdownNav = document.querySelector(".nav-dropdown")
-  dropdownNav.classList.remove("hidden-nav")
-  const hNav = getComputedStyle(dropdownNav).height
-  dropdownNav.classList.add("hidden-nav")
-
+  const getDropNavHeight = () => {
+    dropdownNav.classList.remove("hidden-nav")
+    const hNav = getComputedStyle(dropdownNav).height
+    dropdownNav.classList.add("hidden-nav")
+    return hNav
+  }
   const navSVG = document.querySelector(".nav-container svg")
-  navSVG.addEventListener("click", () => {
-    const [a, b, c] = [...navSVG.querySelectorAll("path")]
+  const [a, b, c] = [...navSVG.querySelectorAll("path")]
+  // const foo = () => {
+  //   console.log("foo")
+  //   const ht = getDropNavHeight()
+  //   dropdownNav.classList.toggle("hidden-nav")
+  //   console.log("to", dropdownNav.classList.contains("hidden-nav"))
+  //   if (dropdownNav.classList.contains("hidden-nav")) {
+  //     dropdownNav.style.height = 0
+  //     a.classList.remove("rotate")
+  //     b.classList.remove("hidden")
+  //     c.classList.remove("rotate-ccw")
+  //   } else {
+  //     dropdownNav.style.height = ht
+  //     dropdownNav.classList.remove("hidden-nav")
+  //     a.classList.add("rotate")
+  //     b.classList.add("hidden")
+  //     c.classList.add("rotate-ccw")
+  //   }
+  // }
+
+  // document.addEventListener("resize", () => {
+  //   navSVG.removeEventListener("click", foo)
+  //   navSVG.addEventListener("click", foo)
+  // })
+
+  window.onresize = () => {
+    const ht = getDropNavHeight()
+    navSVG.onclick = () => {
+      dropdownNav.classList.toggle("hidden-nav")
+      if (dropdownNav.classList.contains("hidden-nav")) {
+        dropdownNav.style.height = 0
+        a.classList.remove("rotate")
+        b.classList.remove("hidden")
+        c.classList.remove("rotate-ccw")
+      } else {
+        dropdownNav.style.height = ht
+        dropdownNav.classList.remove("hidden-nav")
+        a.classList.add("rotate")
+        b.classList.add("hidden")
+        c.classList.add("rotate-ccw")
+      }
+    }
+  }
+
+  const ht = getDropNavHeight()
+  navSVG.onclick = () => {
     dropdownNav.classList.toggle("hidden-nav")
+    console.log("this foo")
     if (dropdownNav.classList.contains("hidden-nav")) {
       dropdownNav.style.height = 0
       a.classList.remove("rotate")
       b.classList.remove("hidden")
       c.classList.remove("rotate-ccw")
     } else {
-      dropdownNav.style.height = hNav
+      dropdownNav.style.height = ht
+      dropdownNav.classList.remove("hidden-nav")
       a.classList.add("rotate")
       b.classList.add("hidden")
       c.classList.add("rotate-ccw")
     }
-  })
+  }
 
   const form = document.querySelector("form")
   const h = getComputedStyle(form).height
